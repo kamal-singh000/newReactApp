498048ee3e48a59068041ab700119c242795990e
kamal-singh000 <kamal.singh@minddeft.net>
2023-03-29 15:33:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,140 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Contractfunction from "./Contractfunction";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import Header from "./Header";
+import Web3 from "web3";
+
+let web3Modal = new Web3Modal({
+  network: "testnet",
+  cacheProvider: true,
+  providerOptions: {},
+});
+let web3 = new Web3(Web3.givenProvider);
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState();
+  const [account, setAccount] = useState(null);
+  const [error, setError] = useState("");
+  const [chainId, setChainId] = useState();
+  const [isConnect, setConnect] = useState(false);
+
+  const connectWallet = async () => {
+    try {
+      const provider = await web3Modal.connect("walletconnect");
+      const library = new ethers.providers.Web3Provider(provider);
+      const signer = await library.getSigner();
+      localStorage.setItem("modalProvider", 1);
+      const accounts = await library.listAccounts();
+      const network = await library.getNetwork();
+      let accBalance = await signer.getBalance();
+      accBalance = ethers.utils.formatEther(accBalance);
+      setProvider(provider);
+      setSigner(signer);
+      if (accounts) {
+        let account = await Web3.utils.toChecksumAddress(accounts[0]);
+        setAccount(account);
+        setConnect(true);
+      }
+      setChainId(network.chainId);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const refreshState = async () => {
+    setAccount();
+    setChainId();
+  };
+  useEffect(() => {
+    (async () => {
+      if (localStorage.getItem("modalProvider")) await connectWallet();
+    })();
+  }, []);
+  const disConnectWallet = async () => {
+    await web3Modal.clearCachedProvider();
+    localStorage.removeItem("modalProvider");
+    refreshState();
+    setConnect(false);
+  };
+  useEffect(() => {
+    (async () => {
+      let data = [];
+      if (chainId !== 0x61 && isConnect) {
+        data = [
+          {
+            chainId: "0x61",
+            chainName: "Smart Chain - Testnet",
+            nativeCurrency: {
+              name: "BNB",
+              symbol: "BNB",
+              decimals: 18,
+            },
+            rpcUrls: ["https://endpoints.omniatech.io/v1/bsc/testnet/public"],
+            blockExplorerUrls: ["https://testnet.bscscan.com"],
+          },
+        ];
+        console.log("chainId", chainId);
+
+        try {
+          await window.ethereum
+            .request({
+              method: "wallet_addEthereumChain",
+              params: data,
+            })
+            .then((resp) => {
+              console.log("resp", resp);
+              connectWallet();
+            });
+        } catch {
+          console.log("catched");
+        }
+      }
+    })();
+  }, [chainId]);
+
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = async (accounts) => {
+        connectWallet();
+
+        if (accounts) {
+          let account = await Web3.utils.toChecksumAddress(accounts[0]);
+          setAccount(account);
+        }
+      };
+
+      const handleChainChanged = (_hexChainId) => {
+        setChainId(_hexChainId);
+      };
+
+      const handleDisconnect = () => {
+        disConnectWallet();
+      };
+
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
+
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
+        }
+      };
+    }
+  }, [provider]);
   return (
-    <div className="bgImg">
-      <Contractfunction />
-    </div>
+    <>
+      <Header
+        isConnect={isConnect}
+        account={account}
+        connectWallet={connectWallet}
+        disConnectWallet={disConnectWallet}
+      />
+    </>
   );
 }
 
