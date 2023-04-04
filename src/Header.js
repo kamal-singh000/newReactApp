2c525abea5a4ef98693b8834c702877564401967
kamal-singh000 <kamal.singh@minddeft.net>
2023-04-04 16:11:17 +0530

    add changes

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import TrustWallet from "./pages/trusrwallet";
+
+const Header = (props) => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg text-bg-danger">
+        <div className="container">
+          <a className="navbar-brand fs-1 text-white" href="#">
+            New React App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <TrustWallet />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
